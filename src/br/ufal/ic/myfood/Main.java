520485a524a665438389a357c764af21b2e501be
johnUFAL <jvdn@ic.ufal.br>
2026-05-05 18:03:12 -0300

    fix: us1_1 was not as requested.

--- src/br/ufal/ic/myfood/Main.java
+++ src/br/ufal/ic/myfood/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         String[] argsEasyAccept = new String[] {
                 "br.ufal.ic.myfood.Facade",
-                "tests/us8_2.txt",
+                "tests/us7_1.txt",
         };
 
         EasyAccept.main(argsEasyAccept);
